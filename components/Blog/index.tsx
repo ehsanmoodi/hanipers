00a43e9f294a1b8226402d47cf2d7f7d388c0d40
ehsanmoodi <ehsanmoodi90@gmail.com>
2022-08-07 03:15:26 +0430

    add animation for blog svg's

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -103,6 +103,85 @@ const Blog: React.FC = () => {
           />
         ))}
       </div>
+
+      <svg
+        className="absolute -top-28 left-32"
+        width="99"
+        height="104"
+        viewBox="0 0 99 104"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.path
+          initial={{ pathLength: 0 }}
+          animate={{ pathLength: 1 }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+          d="M35.4177 103C20.9022 75.5233 46.1254 24.4484 71.8621 24.4483C96.2043 24.4481 106.347 51.0429 90.0942 67.859C62.2913 96.6265 -17.076 72.2668 4.75059 0.999996"
+          stroke="#002147"
+          stroke-width="1.2"
+        />
+      </svg>
+      <svg
+        className="absolute -bottom-20 right-20"
+        width="59"
+        height="67"
+        viewBox="0 0 59 67"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.path
+          initial={{ pathLength: 0 }}
+          animate={{ pathLength: 1 }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+          d="M0.828812 33.6848C15.5389 21.0983 49.8629 30.045 53.3042 45.9899C56.5591 61.0707 41.7987 70.8325 29.4351 62.9629C8.2844 49.5004 12.4343 -2.85656 58.5405 1.345"
+          stroke="#002147"
+          stroke-width="1.2"
+        />
+      </svg>
+      <svg
+        className="absolute right-32 -top-10"
+        width="74"
+        height="66"
+        viewBox="0 0 74 66"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.rect
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 1,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "mirror",
+          }}
+          width="28.3884"
+          height="58.6694"
+          rx="14.1942"
+          transform="matrix(0.721858 -0.692041 -0.692041 -0.721858 42.2622 64.2976)"
+          fill="#FFD66F"
+        />
+        <rect
+          x="0.0178907"
+          y="-0.84834"
+          width="27.1884"
+          height="57.4694"
+          rx="13.5942"
+          transform="matrix(0.721858 -0.692041 -0.692041 -0.721858 50.2939 61.7018)"
+          stroke="#002147"
+          stroke-width="1.2"
+        />
+      </svg>
     </motion.section>
   );
 };
