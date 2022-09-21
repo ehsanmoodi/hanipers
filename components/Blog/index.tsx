9a73eb408c1c5be5878453b5b8a8226edd0a03fa
parsa <parsamirzaee47@gmail.com>
2022-09-21 14:18:50 +0430

    added package conf for server dev - added new blog images

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -1,6 +1,6 @@
-import blogImage1 from "../../public/images/blog/blog-image-1.png";
-import blogImage2 from "../../public/images/blog/blog-image-2.png";
-import blogImage3 from "../../public/images/blog/blog-image-3.png";
+import blogImage1 from "../../public/images/blog/haniblog1.jpg";
+import blogImage2 from "../../public/images/blog/haniblog2.jpg";
+import blogImage3 from "../../public/images/blog/haniblog3.jpg";
 
 import BlogItem from "../BlogItem";
 import H2 from "../H2";
@@ -81,110 +81,110 @@ const Blog: React.FC = () => {
   }, [animation, isInView]);
 
   return (
-    <motion.section
-      variants={blogContainerVariant}
-      initial="hidden"
-      animate={animation}
-      className="index-blog"
-      id="blog"
-      ref={ref}
-    >
-      <H2>OUR BLOG</H2>
-      <div className="index-blog__container">
-        {blogItems.map((item) => (
-          <MotionBlogItem
-            variants={blogItemVariant}
-            key={item.id}
-            image={item.image}
-            title={item.title}
-            date={item.date}
-            views={item.views}
-            href={item.href}
+      <motion.section
+          variants={blogContainerVariant}
+          initial="hidden"
+          animate={animation}
+          className="index-blog"
+          id="blog"
+          ref={ref}
+      >
+        <H2>OUR BLOG</H2>
+        <div className="index-blog__container">
+          {blogItems.map((item) => (
+              <MotionBlogItem
+                  variants={blogItemVariant}
+                  key={item.id}
+                  image={item.image}
+                  title={item.title}
+                  date={item.date}
+                  views={item.views}
+                  href={item.href}
+              />
+          ))}
+        </div>
+
+        <svg
+            className="absolute -top-28 left-6 lg:left-32 scale-50 lg:scale-100"
+            width="99"
+            height="104"
+            viewBox="0 0 99 104"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.path
+              initial={{ pathLength: 0 }}
+              animate={{ pathLength: 1 }}
+              transition={{
+                duration: 2,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              d="M35.4177 103C20.9022 75.5233 46.1254 24.4484 71.8621 24.4483C96.2043 24.4481 106.347 51.0429 90.0942 67.859C62.2913 96.6265 -17.076 72.2668 4.75059 0.999996"
+              stroke="#002147"
+              strokeWidth="1.2"
           />
-        ))}
-      </div>
+        </svg>
 
-      <svg
-        className="absolute -top-28 left-6 lg:left-32 scale-50 lg:scale-100"
-        width="99"
-        height="104"
-        viewBox="0 0 99 104"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          initial={{ pathLength: 0 }}
-          animate={{ pathLength: 1 }}
-          transition={{
-            duration: 2,
-            ease: "easeInOut",
-            repeat: Infinity,
-            repeatType: "loop",
-          }}
-          d="M35.4177 103C20.9022 75.5233 46.1254 24.4484 71.8621 24.4483C96.2043 24.4481 106.347 51.0429 90.0942 67.859C62.2913 96.6265 -17.076 72.2668 4.75059 0.999996"
-          stroke="#002147"
-          strokeWidth="1.2"
-        />
-      </svg>
+        <svg
+            className="absolute -bottom-20 right-20"
+            width="59"
+            height="67"
+            viewBox="0 0 59 67"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.path
+              initial={{ pathLength: 0 }}
+              animate={{ pathLength: 1 }}
+              transition={{
+                duration: 2,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              d="M0.828812 33.6848C15.5389 21.0983 49.8629 30.045 53.3042 45.9899C56.5591 61.0707 41.7987 70.8325 29.4351 62.9629C8.2844 49.5004 12.4343 -2.85656 58.5405 1.345"
+              stroke="#002147"
+              strokeWidth="1.2"
+          />
+        </svg>
 
-      <svg
-        className="absolute -bottom-20 right-20"
-        width="59"
-        height="67"
-        viewBox="0 0 59 67"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          initial={{ pathLength: 0 }}
-          animate={{ pathLength: 1 }}
-          transition={{
-            duration: 2,
-            ease: "easeInOut",
-            repeat: Infinity,
-            repeatType: "loop",
-          }}
-          d="M0.828812 33.6848C15.5389 21.0983 49.8629 30.045 53.3042 45.9899C56.5591 61.0707 41.7987 70.8325 29.4351 62.9629C8.2844 49.5004 12.4343 -2.85656 58.5405 1.345"
-          stroke="#002147"
-          strokeWidth="1.2"
-        />
-      </svg>
-
-      <svg
-        className="absolute right-6 lg:right-32 -top-10"
-        width="74"
-        height="66"
-        viewBox="0 0 74 66"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.rect
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            duration: 1,
-            ease: "easeInOut",
-            repeat: Infinity,
-            repeatType: "mirror",
-          }}
-          width="28.3884"
-          height="58.6694"
-          rx="14.1942"
-          transform="matrix(0.721858 -0.692041 -0.692041 -0.721858 42.2622 64.2976)"
-          fill="#FFD66F"
-        />
-        <rect
-          x="0.0178907"
-          y="-0.84834"
-          width="27.1884"
-          height="57.4694"
-          rx="13.5942"
-          transform="matrix(0.721858 -0.692041 -0.692041 -0.721858 50.2939 61.7018)"
-          stroke="#002147"
-          strokeWidth="1.2"
-        />
-      </svg>
-    </motion.section>
+        <svg
+            className="absolute right-6 lg:right-32 -top-10"
+            width="74"
+            height="66"
+            viewBox="0 0 74 66"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.rect
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 1,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatType: "mirror",
+              }}
+              width="28.3884"
+              height="58.6694"
+              rx="14.1942"
+              transform="matrix(0.721858 -0.692041 -0.692041 -0.721858 42.2622 64.2976)"
+              fill="#FFD66F"
+          />
+          <rect
+              x="0.0178907"
+              y="-0.84834"
+              width="27.1884"
+              height="57.4694"
+              rx="13.5942"
+              transform="matrix(0.721858 -0.692041 -0.692041 -0.721858 50.2939 61.7018)"
+              stroke="#002147"
+              strokeWidth="1.2"
+          />
+        </svg>
+      </motion.section>
   );
 };
 
